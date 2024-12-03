b10d47bf6943bc4324017f9d7f8f3062a5c4d947
RYANFRANKLIN SUMBELE <Ngometune@gmail.com>
2024-12-03 19:43:52 +0100

    CRUD Laravel

---
+++ studentmanagement/resources/views/enrollments/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+@section('content')
+    <div class="card">
+        <div class="card-header">Enrollment</div>
+        <div class="card-body">
+
+
+            <div class="card-body">
+                <h5 class="card-title">Enrollment Number : {{ $enrollments->enroll_no }}</h5>
+                <p class="card-text">Batch : {{ $enrollments->batch->name }}</p>
+                <p class="card-text">Student : {{ $enrollments->student->name }}</p>
+                <p class="card-text">Join date : {{ $enrollments->join_date }}</p>
+                <p class="card-text">fee : {{ $enrollments->fee }}</p>
+            </div>
+
+            </hr>
+
+        </div>
+    </div>
+@endsection
---
+++ studentmanagement/resources/views/payments/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+@section('content')
+    <div class="card">
+        <div class="card-header">Payment</div>
+        <div class="card-body">
+
+
+            <div class="card-body">
+                <h5 class="card-text">Enrollment No : {{ $payments->enrollment->enroll_no }}</h5>
+                <p class="card-text">Paid Date : {{ $payments->paid_date }}</p>
+                <p class="card-text">Amount : {{ $payments->amount }}</p>
+            </div>
+
+            </hr>
+
+        </div>
+    </div>
+@endsection
